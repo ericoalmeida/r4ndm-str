3b9384dd6f7518f32d9877489ecc27e1ab15e5a9
Erico Almeida <ericoalmeida.egr@gmail.com>
2022-12-17 15:14:48 -0400

    test: ensure numbers is mapped correctly

---
+++ tests/mappers/map-numbers.spec.ts
@@ -0,0 +1,26 @@
+import { characters } from '@src/constants/characters'
+import { mapNumbers } from '@src/mappers'
+
+describe('mapNumbers', () => {
+  it('Should map numbers correctly', () => {
+    const chars = ''
+    const useNumbers = true
+
+    const expectedMappedChars = characters.numbers
+
+    const mappedChars = mapNumbers(useNumbers, chars)
+
+    expect(mappedChars).toBe(expectedMappedChars)
+  })
+
+  it('Should not map numbers if not use them', () => {
+    const chars = ''
+    const useNumbers = false
+
+    const expectedMappedChars = chars
+
+    const mappedChars = mapNumbers(useNumbers, chars)
+
+    expect(mappedChars).toBe(expectedMappedChars)
+  })
+})
